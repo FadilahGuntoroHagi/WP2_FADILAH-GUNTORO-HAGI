681ba2465661d513a953a6c7f301d5214eaf25c1
FadilahGuntoroHagi <hadgie100@gmail.com>
2021-10-27 13:16:14 +0700

    latihan1_12200385

---
+++ application/views/beranda.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+	<tittle></tittle>
+</head>
+<body>
+	<h2>Hai Nama Saya Adalah Fadilah Guntoro Hagi</h2>
+</body>
+</html>
